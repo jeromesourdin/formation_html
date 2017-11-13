03e42c1fbd4f1e6676a3f35d56b76744a8b72741
jeromesourdin <jerome.sourdin@cotesdarmor.fr>
2017-11-13 09:43:16 +0100

    ok

---
+++ html5/exercice30.js
@@ -0,0 +1,22 @@
+var mesP = document.querySelectorAll("p");
+console.log(mesP);
+
+var firstChild = document.querySelector("#p1").firstChild;
+console.log("Premier Enfant : " + firstChild.nodeName);
+
+document.querySelector("#bouton").onclick = function ()
+{
+	var monP = document.querySelector ("#p1");
+	monP.textContent = "t'es cuit !";
+
+	var mesPPair = document.querySelectorAll("p.pair");
+	for (var i = 0; mesPPair.length > i; i++) {
+		mesPPair[i].style.display = "none";
+	}
+
+	document.querySelector("img").className = "hidden";
+};
+
+
+
+//
