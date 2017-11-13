78607ce6f10db33869ab56fe602b857e32c4eab7
jeromesourdin <jerome.sourdin@cotesdarmor.fr>
2017-11-13 09:11:19 +0100

    ok

---
+++ exercice28.js
@@ -0,0 +1,15 @@
+function disBonjour()
+{
+	console.log("Bijour missieur Vincent !");
+}
+var i =0;
+function messageSouris()
+{
+	i++;
+	console.log("Vous passez sur l'image : " + i + " fois");
+
+}
+function messageBeau()
+{
+	console.log("C'est beau !!!!!");
+}
