7a0c9834c0aab3944f41015978fb540a3913f33a
jeromesourdin <jerome.sourdin@cotesdarmor.fr>
2017-11-13 09:11:29 +0100

    ok

--- exercice24.js
+++ exercice24.js
@@ -10,3 +10,45 @@
 // - avancer() qui passe l'etat a avance
 // - arreter()
 // - reculer()
+var voitureFelix = {"marque":"dacia", "model":"duster","motorisation":"2.1 TDi","etat":""};
+
+voitureFelix.getDescription = function() 
+{
+	switch (this.etat)
+	{
+		case "arreter":
+			var finDesc = "est arreté";
+			break;
+		case "avancer":
+			var finDesc = "avance";
+			break;
+		case "reculer":
+			var finDesc = "recule";
+			break;
+	}
+	return "C'est un " + this.model + " " + this.marque + " (" + this.motorisation + ") qui " + finDesc;	
+}
+
+voitureFelix.avancer = function()
+{
+	this.etat = "avancer";
+}
+
+voitureFelix.reculer = function()
+{
+	this.etat = "reculer";
+}
+
+voitureFelix.arreter = function()
+{
+	this.etat = "arreter";
+}
+
+voitureFelix.avancer();
+console.log(voitureFelix.getDescription());
+voitureFelix.arreter();
+console.log(voitureFelix.getDescription());
+voitureFelix.reculer();
+console.log(voitureFelix.getDescription());
+voitureFelix.arreter();
+console.log(voitureFelix.getDescription());
--- exercice27.js
+++ exercice27.js
@@ -1,54 +1,14 @@
-// Définir une variable voitureFelix
-// qui a pour proprietes : 
-// - marque
-// - model
-// - motorisation
-// - etat (arret, avance, recul)
-// et qui a methodes
-// - getDescription() 
-// qui renvoie marque+model+motorization+etat
-// - avancer() qui passe l'etat a avance
-// - arreter()
-// - reculer()
-var voitureFelix = {"marque":"dacia", "model":"duster","motorisation":"2.1 TDi","etat":""};
+var message ='{"nom":"jerome"}';
 
-voitureFelix.getDescription = function() 
-{
-	switch (this.etat)
-	{
-		case "arreter":
-			var finDesc = "est arreté";
-			break;
-		case "avancer":
-			var finDesc = "avance";
-			break;
-		case "reculer":
-			var finDesc = "recule";
-			break;
-	}
-	return "C'est un " + this.model + " " + this.marque + " (" + this.motorisation + ") qui " + finDesc;	
-}
+console.log(message);
+console.log(message.nom);
 
-voitureFelix.avancer = function()
-{
-	this.etat = "avancer";
-}
+var utilisateur=JSON.parse(message);
+console.log(utilisateur);
+console.log(utilisateur.nom);
 
-voitureFelix.reculer = function()
-{
-	this.etat = "reculer";
-}
+utilisateur.age = 41;
+console.log(utilisateur);
 
-voitureFelix.arreter = function()
-{
-	this.etat = "arreter";
-}
-
-voitureFelix.avancer();
-console.log(voitureFelix.getDescription());
-voitureFelix.arreter();
-console.log(voitureFelix.getDescription());
-voitureFelix.reculer();
-console.log(voitureFelix.getDescription());
-voitureFelix.arreter();
-console.log(voitureFelix.getDescription());
+messageAEnvoyer = JSON.stringify(utilisateur);
+console.log(messageAEnvoyer)
